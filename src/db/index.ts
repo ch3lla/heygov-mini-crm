0be0efd758245854978841def2cd0f34178615a8
Chella <chukwumaemmanuella03@gmail.com>
2025-11-30 12:34:00 +0100

    update: camel case uniform

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,11 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise" // /promise supports promise requests, drizzle requires a promise request
 
-const mysql_connection = await mysql.createConnection({
+const mysqlConnection = await mysql.createConnection({
     host: process.env.DATABASE_HOST!,
     user: process.env.DATABASE_USER!,
     database: process.env.DATABASE_NAME!,
     password: process.env.DATABASE_PASSWORD!,
 });
 
-export const db = drizzle({ client: mysql_connection, /* logger: true  */});
+export const db = drizzle({ client: mysqlConnection, /* logger: true  */});
