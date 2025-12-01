3e902b6ce1d96a2f912b1e13fef8a6cbcc1440a0
Chella <chukwumaemmanuella03@gmail.com>
2025-12-01 22:33:36 +0100

    update: fix did not work

--- src/services/email/index.ts
+++ src/services/email/index.ts
@@ -1,9 +1,7 @@
-import 'dotenv/config';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  port: Number(process.env.EMAIL_PORT),
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
