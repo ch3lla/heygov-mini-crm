f29cc22ebe67dfe0b8ba352a9a3dc7f5a070f145
Chella <chukwumaemmanuella03@gmail.com>
2025-12-01 15:52:21 +0100

    updae: using production/live db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,39 @@
+import path from "path";
+import { fileURLToPath } from 'url';
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise" // /promise supports promise requests, drizzle requires a promise request
+import mysql from "mysql2/promise";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename);
+
+const caCertPath = path.resolve(__dirname, '../../ca.pem');
+const caCert = fs.readFileSync(caCertPath, 'utf-8');
 
 const mysqlConnection = await mysql.createConnection({
-    host: process.env.DATABASE_HOST!,
-    user: process.env.DATABASE_USER!,
-    database: process.env.DATABASE_NAME!,
-    password: process.env.DATABASE_PASSWORD!,
+  uri: process.env.DATABASE_URL!, 
+  ssl: {
+    ca: caCert, 
+    rejectUnauthorized: true, 
+  },
 });
+
+
+
+// // const caBuffer = Buffer.from(process.env.DB_CA!, "base64");
+
+// const caPath = path.resolve(__dirname, "../ca.pem"); 
+// const mysqlConnection = await mysql.createConnection({
+//     // host: process.env.DATABASE_HOST!,
+//     // user: process.env.DATABASE_USER!,
+//     // database: process.env.DATABASE_NAME!,
+//     // password: process.env.DATABASE_PASSWORD!,
+//     uri: process.env.DATABASE_URL!,
+//     ssl: {
+//         ca: fs.readFileSync(caPath)
+//     },
+//     connectTimeout: 10000,
+// });
 
 export const db = drizzle({ client: mysqlConnection, /* logger: true  */});
