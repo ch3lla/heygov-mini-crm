7730fa8307b50c3c43f263ccdadcf46b8348da35
Chella <chukwumaemmanuella03@gmail.com>
2025-12-02 17:52:36 +0100

    fix: moved smtp from nodemail to resend

--- src/services/email/index.ts
+++ src/services/email/index.ts
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import nodemailer from 'nodemailer';
+import { Resend } from "resend";
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -10,13 +12,28 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async (to: string, subject: string, html: string) => {
   try {
-    const info = await transporter.sendMail({
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    const { data, error } = await resend.emails.send({
       from: `"HeyGov Assistant" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       html,
     });
-    console.log("Email sent: %s", info.messageId);
+
+    if (error) {
+      console.error("Error sending email:", error);
+      return false
+    } else {
+      console.log("Email sent: %s", data.id);
+      return true
+    }
+    // const info = await transporter.sendMail({
+    //   from: `"HeyGov Assistant" <${process.env.EMAIL_USER}>`,
+    //   to,
+    //   subject,
+    //   html,
+    // });
+    // console.log("Email sent: %s", info.messageId);
     return true;
   } catch (error) {
     console.error("Error sending email:", error);
