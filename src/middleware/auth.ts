c7ca675978fb0bb6b234ac027e4df403364744fd
Chella <chukwumaemmanuella03@gmail.com>
2025-12-09 17:53:15 +0100

    update: modifed the auth middleware to adapt to SSE token requirements

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,15 +5,26 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
 
 export const authenticate = (req: IRequest, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  // const authHeader = req.headers.authorization;
+  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  //   return res.status(401).json({ 
+  //       status: "Error",
+  //       message: "Unauthorized"
+  //   });
+  // }
+
+  // const token = authHeader.split(" ")[1];
+
+  let token = req.headers.authorization?.split(" ")[1];
+
+  if (!token && req.query.token) {
+      token = req.query.token as string;
+  } else if (!token && !req.query.token) {
     return res.status(401).json({ 
         status: "Error",
         message: "Unauthorized"
     });
   }
-
-  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token!, JWT_SECRET) as IJwtPayload;
