dc6256bfb2190634fe5456295d17ceefffb268d4
Chella <chukwumaemmanuella03@gmail.com>
2026-01-31 07:54:12 +0100

    Refactor MySQL connection to use DATABASE_URL

--- src/db/index.ts
+++ src/db/index.ts
@@ -43,10 +43,7 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise" // /promise supports promise requests, drizzle requires a promise request
 
 const mysql_connection = await mysql.createConnection({
-    host: process.env.DATABASE_HOST!,
-    user: process.env.DATABASE_USER!,
-    database: process.env.DATABASE_NAME!,
-    password: process.env.DATABASE_PASSWORD!,
+    uri: process.env.DATABASE_URL!
 });
 
 export const db = drizzle({ client: mysql_connection });
